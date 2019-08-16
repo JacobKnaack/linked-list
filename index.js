24d1844fe600fdbc6a4a80721654839be4748514
JacobKnaack <jacobknck@gmail.com>
2019-08-16 16:00:14 -0700

    adds methods and tests for linked lists

--- index.js
+++ index.js
@@ -11,22 +11,66 @@ class LinkedList {
     const newNode = new Node(data);
     if (this.head === null) {
       this.head = newNode;
+      return this.head;
     } else {
       let current = this.head;
       while (current.next !== null) {
         current = current.next;
       }
       current.next = newNode;
+      current.next.previous = current;
+      return current.next;
     }
   }
 
 
-  get(pointer) {
-
+  get(index) {
+    if (index > -1) {
+      let current = this.head;
+      let i = 0;
+      while ((current !== null) && (i < index)) {
+        current = current.next;
+        i++;
+      }
+      return current !== null ? current.data : undefined;
+    } else {
+      return undefined;
+    }
   }
 
-  listNodes() {
+  traverse() {
+    let current = this.head;
+    let result = {};
+    while (current !== null) {
+      current = current.next;
+      result[current.data] = current;
+    }
 
+    return result;
+  }
+
+  includes(value) {
+    let current = this.head;
+    while (current !== null) {
+      current = current.next;
+      if (current.data) {
+        return current;
+      }
+    }
+
+    return undefined;
+  }
+
+  remove(value) {
+    let current = this.head;
+    while (current !== null) {
+      if (current.data === value) {
+        this.head = current.next;
+        return current;
+      }
+    }
+
+    return undefined;
   }
 }
 
--- test/linkedList.test.js
+++ test/linkedList.test.js
@@ -3,13 +3,31 @@
 const LinkedList = require('../');
 
 describe('testing the linked list module', () => {
-  test('module produces a linked list', () => {
+  test('Module produces a linked list', () => {
     const List = new LinkedList(1);
-    expect(typeof List).toBe('object');
+    expect(List.head).exists;
+    expect(List.head.data).exists;
+    expect(List.head.next).exists;
     expect(typeof List.head).toBe('object');
   });
 
-  // test('module can add nodes to an existing Linked List', () => {
-  //   const List = new LinkedList()
-  // });
+  test('Module can insert nodes', () => {
+    const List = new LinkedList();
+    expect(List.insert(200).data).toEqual(200);
+    expect(List.insert(1).data).toEqual(1);
+    expect(typeof List.head.next).toBe('object');
+    expect(typeof List.head.next.previous).toBe('object');
+  });
+
+  test('Module can select an index if it exists', () => {
+    const List = new LinkedList(1);
+    expect(List.get(0)).toEqual(1);
+    expect(List.get(1)).toBe(undefined);
+  });
+
+  test('Module can remove nodes', () => {
+    const List = new LinkedList(1);
+    expect(List.remove(1)).toEqual({ data: 1, next: null, previous: null });
+    expect(List).toEqual({ head: null });
+  })
 });
